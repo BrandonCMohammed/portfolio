7882da94ad8f0f17bd39ae663b0b73f77da1a43a
Brandon Mohammed <brandonmohammed711@gmail.com>
2025-01-29 15:59:26 -0400

    Split the header and added intro text

--- portfolio/app/components/footer.tsx
+++ portfolio/app/components/footer.tsx
@@ -2,7 +2,7 @@ export default function Footer()
 {
     return(
         <>
-            <div>
+            <div className="bg-red-500 flex justify-center items-center h-96">
                 Footer
             </div>
         </>
--- portfolio/app/components/header.tsx
+++ portfolio/app/components/header.tsx
@@ -1,9 +1,16 @@
-export default function Header()
-{
-    return(
+export default function Header() {
+    return (
         <>
-            <div>
-                Header
+            <div className="flex justify-center items-center h-[50rem]">
+                <div className="bg-red-400 w-full h-full flex justify-center items-center">
+                    <p className="text-7xl pl-2">
+                        Hi, I am Brandon Mohammed and I am a software developer
+                    </p>
+                </div>
+                
+                <div className="bg-red-500 w-full h-full flex justify-center items-center">
+                    PIC
+                </div>
             </div>
         </>
     );
