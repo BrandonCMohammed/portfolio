c03bc84c5f9c04cd37522351dd58fe1896365a40
Brandon Mohammed <brandonmohammed711@gmail.com>
2025-01-29 15:17:11 -0400

    Created the Header and Footer components and added them to the layout file

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -1,3 +1,22 @@
 export default function Home() {
-  return <div className="">HELLO WORLD!!</div>;
+  return (
+  
+  
+  <div className="">
+
+      <div id="intro about me">
+        Intro
+      </div>
+
+      <div id="projects">
+        Projects
+
+      </div>
+
+      <div id="skills">
+        Skills
+      </div>
+      
+
+  </div>);
 }
