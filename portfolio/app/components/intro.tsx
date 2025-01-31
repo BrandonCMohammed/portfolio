8503c878ee7c3a65b8c8d4e0cda4f98b8f16c33a
Brandon Mohammed <brandonmohammed711@gmail.com>
2025-01-31 15:29:21 -0400

    added some content to the intro conponent

--- portfolio/app/components/intro.tsx
+++ portfolio/app/components/intro.tsx
@@ -1,8 +1,35 @@
 export default function Intro() {
     return (
         <>
-            <section id="intro_about_me" className="h-96 border-2 border-red-400 flex justify-center items-center w-full">
-                Intro
+            <hr/>
+            <section id="intro_about_me" className="h-96 border-2 border-red-400 flex justify-center items-center w-full px-[40em]">
+
+                
+                        Hi, I'm Brandon Mohammed and I am a software developer from Trinidad and Tobago. 
+                        I have graduated from the University of the West Indies with a BSc in Computer Science and Physics.
+                        {/* <hr/> */}
+
+                   
+
+                
+
+                    I have experience in Web Development, UI design and Database Design.
+                    In my current position, I am being trained in Enterprise Architecture and Developer Operations. 
+                    {/* <hr/> */}
+
+                
+
+                    I am a passionate, creative and patient person who loves to problem solve. Some of my hobbies that show this is my passion for speed cubing and music.
+                    I am a quick learner and honest. I pay attention to details which allows me to plan and stay well organized. I am a team player and my 
+                    communication skills as well as open mindedness and flexability aid me in working with teams. Due to me experience working under an Enterprise Architecture team,
+                    this has allowed me to develop a business prespective to go along with my technical mindset. 
+                    {/* <hr/> */}
+
+                
+                    I am excited to work with you. If you need to contact me, my number is +1(868)314-9365 and my email is brandonmohammed711@gmail.com
+
+                
+                
 
             </section>
         </>
