1162162996cd40b6a3e6991a2e385ef1e01a6278
Brandon Mohammed <brandonmohammed711@gmail.com>
2025-02-10 08:44:16 -0400

    Made the intro section fade in

--- portfolio/app/components/header.tsx
+++ portfolio/app/components/header.tsx
@@ -16,11 +16,13 @@ export default function Header() {
         return () => clearTimeout(timer);
     }, [])
 
+    
+
     return (
         <>
             <div className="flex flex-col-reverse md:flex-row justify-center items-center h-screen">
                 <div className=" w-full h-full flex flex-col justify-center ">
-                    <div className={`transition-opacity duration-1000 ${visible ? 'opacity-100' : 'opacity-0'}`} >
+                    <div className={`transition-opacity duration-1000 ease-in ${visible ? 'opacity-100' : 'opacity-0'}`} >
                         <p className="text-7xl px-5 mb-10 leading-11 flex justify-center items-center ">
                             Hi, I am
                             {visible}
@@ -32,8 +34,8 @@ export default function Header() {
 
                     <hr className="w-[55rem] h-1 mx-auto my-4 bg-gray-100 border-0 rounded-sm md:my-10 dark:bg-gray-700" />
 
-                    <div className={`transition-opacity duration-[5s] ${visible ? 'opacity-100' : 'opacity-0'}`}>
-                        <div className={`transform transition-transform duration-[4s] ${visible ? 'translate-y-0' : 'translate-y-10'}`}>
+                    <div className={`transition-opacity duration-[4s] ${visible ? 'opacity-100' : 'opacity-0'}`}>
+                        <div className={`transform transition-transform duration-[2s] ${visible ? 'translate-y-0' : 'translate-y-12'}`}>
                             <div className="flex flex-col md:flex-row justify-around items-center w-full  mt-10 px-2">
                                 <div >
                                     <Link href="#intro_about_me" className="rounded-full  text-3xl px-4 py-2 ">
--- portfolio/app/components/intro.tsx
+++ portfolio/app/components/intro.tsx
@@ -1,35 +1,71 @@
+"use client"
+import { useState, useEffect, useRef } from "react";
+
 export default function Intro() {
+
+    const [isVisible, setIsVisible] = useState(false);
+    const elementRef = useRef(null)
+
+    useEffect(() => {
+
+        const observer = new IntersectionObserver(([entry], observer) => {
+            if (entry.isIntersecting) {
+                setIsVisible(true)
+            }
+            console.log(entry)
+        }, { threshold: 0.5 })
+
+        if (elementRef.current) {
+            observer.observe(elementRef.current);
+        }
+
+        console.log(elementRef.current)
+        return () => {
+            if (elementRef.current) {
+                observer.unobserve(elementRef.current)
+            }
+        }
+    }, [])
     return (
-        <>
 
-            <section id="intro_about_me" className=" flex flex-col  w-full text-gray-300 p-10  bg-neutral-800">
-                <div className="flex justify-center items-center ">
-                    <p className="text-5xl ">Introduction</p>
+        <section ref={elementRef} id="intro_about_me" className=" flex flex-col text-gray-300 p-10 w-full bg-neutral-800">
+
+            <div className={`transform transition-transform duration-[3s] ease-in-out w-full ${isVisible ? 'translate-x-0' : '-translate-x-[10rem]'}`}>
+
+                <div className={`transition-opacity duration-[2s] ease-in ${isVisible ? 'opacity-100' : 'opacity-0'}`}>
+
+
+                    <div className="flex justify-center items-center w-full">
+                        <p className="text-5xl ">Introduction</p>
+                    </div>
+                    <div className="flex flex-col border rounded-xl p-5">
+
+                        <div className="flex flex-row -mb-2  ">
+                            <div className="w-[55.8rem] flex justify-center items-center px-5 border border-indigo-500 bg-slate-800 text-3xl rounded-xl ">
+                                Software developer based in Trinidad and Tobago
+                            </div>
+                            <div className="w-1/2 h-80"></div>
+                        </div>
+                        <div className="flex flex-row -mb-2">
+                            <div className="w-1/2 h-80"></div>
+                            <div className="w-[55.8rem] flex justify-center items-center px-5 border border-indigo-500 bg-slate-800 text-3xl rounded-xl">
+                                Experience in Web Development, UI design and Database Design.
+                            </div>
+                        </div>
+                        <div className="flex flex-row">
+                            <div className="w-[55.8rem] flex justify-center items-center px-5 border border-indigo-500 bg-slate-800 text-3xl rounded-xl">
+                                Passionate, creative, problem solver, team player, business mindset
+                            </div>
+                            <div className="w-1/2 h-80"></div>
+                        </div>
+
+                    </div>
                 </div>
-                <div className="flex flex-col border rounded-xl p-5">
+            </div>
 
-                    <div className="flex flex-row -mb-2  ">
-                        <div className="w-[55.8rem] flex justify-center items-center px-5 border border-indigo-500 bg-slate-800 text-3xl rounded-xl ">
-                            Software developer based in Trinidad and Tobago
-                        </div>
-                        <div className="w-1/2 h-80"></div>
-                    </div>
-                    <div className="flex flex-row -mb-2">
-                        <div className="w-1/2 h-80"></div>
-                        <div className="w-[55.8rem] flex justify-center items-center px-5 border border-indigo-500 bg-slate-800 text-3xl rounded-xl">
-                            Experience in Web Development, UI design and Database Design.
-                        </div>
-                    </div>
-                    <div className="flex flex-row">
-                        <div className="w-[55.8rem] flex justify-center items-center px-5 border border-indigo-500 bg-slate-800 text-3xl rounded-xl">
-                            Passionate, creative, problem solver, team player, business mindset
-                        </div>
-                        <div className="w-1/2 h-80"></div>
-                    </div>
 
-                </div>
 
-            </section>
-        </>
+        </section>
+
     );
 }
